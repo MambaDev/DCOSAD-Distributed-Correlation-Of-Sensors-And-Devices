f042bedd2d85205b2e21a8830964a055c6cfd01c
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-02-20 13:57:53 +0000

    fix: working nsq and docker with nodejs

--- aggregator/index.js
+++ aggregator/index.js
@@ -11,18 +11,28 @@ const state = {
   readerReady: false,
 };
 
-const reader = new nsq.Reader('data', 'progressed', {
-  lookupdHTTPAddresses: 'nsqlookupd:4161',
-});
+// ###################################
+// # NSQ
+// ###################################
 
-reader.connect();
-const writer = new nsq.Writer('nsqd', 4150);
-
-const handleReadyState = (type) => {
+/**
+ * Marks a handler (reader, writer) as ready in the system and logs that its ready.
+ * @param {string} type The type of nsq handler that is ready.
+ */
+function handleReadyState(type) {
   logger.info(`${type} ready`);
   state[`${type}Ready`] = true;
-};
+}
 
+const writer = new nsq.Writer('nsqd', 4150);
+const reader = new nsq.Reader('data', 'progressed', {
+  lookupdHTTPAddresses: 'nsqlookpupd:4161',
+});
+
+/**
+ * Handles the ready state handlers for the reader and the writer, allows the setup and trigger of
+ * internal trackers to know if and when the handlers are ready for action.
+ */
 writer.on('ready', () => handleReadyState('writer'));
 reader.on('ready', () => handleReadyState('reader'));
 
@@ -30,6 +40,10 @@ reader.on('message', (msg) => {
   console.log('Received message [%s]: %s', msg.id, msg.body.toString());
   msg.finish();
 });
+
+// ###################################
+// # Restify
+// ###################################
 
 const server = restify.createServer();
 
@@ -45,9 +59,16 @@ server.get('/data', (req, res) => {
 
 server.listen(8080, () => console.log('%s listening at %s', server.name, server.url));
 
+// ###################################
+// # Setup
+// ###################################
+
 async function setup() {
+  await sleep(1000);
   logger.info('setting up aggregator');
+
   reader.connect();
+  writer.connect();
 }
 
 setup();
